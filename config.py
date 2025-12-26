1eca8e840c5958befadb5d18855bbc4acd771821
kamikaze12 <33387860+kamikaze12@users.noreply.github.com>
2025-12-26 09:44:01 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+config.py
+TELEGRAM_API_ID = 'YOUR_API_ID'  # Dari my.telegram.org
+TELEGRAM_API_HASH = 'YOUR_API_HASH'
+TELEGRAM_CHANNELS = ['BinanceKillers', 'wolfxsignals']  # List channel Telegram (username tanpa @)
+
+X_ACCOUNTS = ['BinanceKillers', 'wolfxsignals', 'signal0x']  # List akun X (username tanpa @)
+
+BOT_TOKEN = 'YOUR_BOT_TOKEN'  # Dari @BotFather
+CHAT_ID = 'YOUR_CHAT_ID'  # ID Telegram kamu
+
+CONFIDENCE_THRESHOLD = 70  # Kirim kalau skor > ini
+CHECK_INTERVAL = 1800  # Detik (30 menit)
